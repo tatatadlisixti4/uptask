3ce1d3ab87632adb5187644a9acb160e5e9720bc
Tatatadlisixti4 <tg.ibarra@gmail.com>
2024-11-12 12:34:44 -0300

    Creando la Vista de Dashboard

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -5,6 +5,7 @@ use MVC\Router;
 
 class DashboardController {
     public static function index(Router $router) {
+        session_start();
         $router->render('dashboard/index',  [
             
         ]);
--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,1 +1,7 @@
 <h1>Desde Dashboard</h1>
+<div class="dashboard">
+    <?php include_once __DIR__ . '/../templates/sidebar.php' ;?>
+    <div class="principal">
+        <?php include_once __dir__ . '/../templates/barra.php' ;?>
+    </div>
+</div>
