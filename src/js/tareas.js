3bf3bba5fd590f717289419a4452ec00f7b29177
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-17 01:56:07 -0300

    Comenzando el scripting para mostrar las tareas

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -25,8 +25,27 @@
     }
 
     function mostrarTareas(tareas) {
-        console.log(tareas);
-        
+        if(tareas.length === 0) {
+            const contenedorTareas = $('#listado-tareas');
+            const textoNoTareas = document.createElement('LI');
+            textoNoTareas.textContent = 'No hay tareas';
+            textoNoTareas.classList.add('no-tareas');
+            contenedorTareas.appendChild(textoNoTareas);
+            return;
+        }
+        tareas.forEach(tarea => {
+            const contenedorTarea = document.createElement('LI');
+            contenedorTarea.dataset.tareaId = tarea.id;
+            contenedorTarea.classList.add('tarea');
+            const nombreTarea = document.createElement('P');
+            nombreTarea.textContent = tarea.nombre;
+            console.log(nombreTarea.textContent);
+            
+            console.log(nombreTarea);
+            
+            
+            
+        });
     }
 
     function mostrarFormulario() {
