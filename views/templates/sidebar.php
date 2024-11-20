454c1abbdc7cd529a5f2f5a5066807f0cb5757a3
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-19 22:26:26 -0300

    Creando el botón para cerrar sesión

--- views/templates/sidebar.php
+++ views/templates/sidebar.php
@@ -6,10 +6,13 @@
         </div>
     </div>
 
-
     <nav class="sidebar-nav">
         <a class="<?php echo($titulo === 'Proyectos')? 'activo' : '' ;?>" href="/dashboard">Proyectos</a>
         <a class="<?php echo($titulo === 'Crear Proyecto')? 'activo' : '' ;?>" href="/crear-proyecto">Crear Proyecto</a>
         <a class="<?php echo($titulo === 'Perfil')? 'activo' : '' ;?>" href="/perfil">Perfil</a>
     </nav>
+
+    <div class="cerrar-sesion-mobile">
+        <a href="/logout" class="cerrar-sesion">Cerrar Sesión</a>
+    </div>
 </aside>
