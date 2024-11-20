e92a9c9289245009914726d96231e0650df1dbc9
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-19 22:08:49 -0300

    Mostrando y ocultando el menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,23 @@
 const $ = selector => document.querySelector(selector);
 const $$ = selector => document.querySelectorAll(selector);
 const $$$ = (element, event, handler) => element.addEventListener(event, handler);
-
 const $$$$ = (father, son) => father.querySelector(son);
+
 const sidebar = $('.sidebar');
 const mobileMenuBtn = $('#mobile-menu');
+const cerrarMenuBtn = $('#cerrar-menu');
 if(mobileMenuBtn) {
      $$$(mobileMenuBtn, 'click', function () {
-         sidebar.classList.toggle('mostrar');
+         sidebar.classList.add('mostrar');
      });
-}0
+}
+
+if(cerrarMenuBtn) {
+    $$$(cerrarMenuBtn, 'click', function () {
+        sidebar.classList.add('ocultar');
+        setTimeout(() => {
+            sidebar.classList.remove('mostrar');
+            sidebar.classList.remove('ocultar');
+        }, 1000)
+    });
+}
--- views/templates/sidebar.php
+++ views/templates/sidebar.php
@@ -1,5 +1,11 @@
 <aside class="sidebar">
-    <h2>UpTask</h2>
+    <div class="contenedor-sidebar">
+        <h2>UpTask</h2>
+        <div class="cerrar-menu">
+            <img src="build/img/cerrar.svg" alt="imagen cerrar menu" id="cerrar-menu">
+        </div>
+    </div>
+
 
     <nav class="sidebar-nav">
         <a class="<?php echo($titulo === 'Proyectos')? 'activo' : '' ;?>" href="/dashboard">Proyectos</a>
